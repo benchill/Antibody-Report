c7323f51a30e6d7da478a23e438d2d02c7264fd7
Ben Hill <ben_c_hill@hotmail.com>
2012-08-21 08:10:36 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function processForm() {  
     var antibodies = ["D", "C", "E", "c", "e", "K", "Kpa", "Jsa", "k", "Kpb", "Jsb", "Fya", "Fyb", "Jka", "Jkb", "M", "N", "S", "s", "P", "Lea", "Leb", "Lua", "Lub"];
-    var percentComp = [document.antigenForm.compD.value, document.antigenForm.compC.value, document.antigenForm.compE.value, document.antigenForm.compc.value, document.antigenForm.compe.value, document.antigenForm.compK.value, document.antigenForm.compKpa.value, document.antigenForm.compJsa.value, document.antigenForm.compk.value, document.antigenForm.compKpb.value, document.antigenForm.compJsb.value, document.antigenForm.compFya.value, document.antigenForm.compFyb.value, document.antigenForm.compJka.value, document.antigenForm.compJkb.value, document.antigenForm.compM.value, document.antigenForm.compN.value, document.antigenForm.compN.value, document.antigenForm.compS.value, document.antigenForm.comps.value, document.antigenForm.compP.value, document.antigenForm.compLea.value, document.antigenForm.compLeb.value, document.antigenForm.compLua.value, document.antigenForm.compLub.value];
+    var i, percentComp;
+    percentComp = [];
+    for(i = 0; i < antibodies.length; i += 1) {
+        percentComp[i] = document.antigenForm['comp' + antibodies[i]].value;
+    }
+    //  var percentComp = [document.antigenForm.compD.value, document.antigenForm.compC.value, document.antigenForm.compE.value, document.antigenForm.compc.value, document.antigenForm.compe.value, document.antigenForm.compK.value, document.antigenForm.compKpa.value, document.antigenForm.compJsa.value, document.antigenForm.compk.value, document.antigenForm.compKpb.value, document.antigenForm.compJsb.value, document.antigenForm.compFya.value, document.antigenForm.compFyb.value, document.antigenForm.compJka.value, document.antigenForm.compJkb.value, document.antigenForm.compM.value, document.antigenForm.compN.value, document.antigenForm.compN.value, document.antigenForm.compS.value, document.antigenForm.comps.value, document.antigenForm.compP.value, document.antigenForm.compLea.value, document.antigenForm.compLeb.value, document.antigenForm.compLua.value, document.antigenForm.compLub.value];
     var tempMatrix= new Array();
     var patientAntibodies=new Array();
     var compUnitsA = new Array();
@@ -9,12 +14,34 @@ function processForm() {
     var pregnant = document.antigenForm.pregnant;
     var compUnits = new Array();
     var percentCompatible = 1;
-    var i;
+//    var i;
     var currentlyPregnant = false;
 //    var comp = new Array();
     var patientSexA=0;
     var numberAntibodies;
     var n=0;
+// from Sean W.
+        conjoin = function (x) {
+     // This function joins the elements of an array using the "Oxford comma".
+        if ((x instanceof Array) === false) {
+            throw new TypeError('Argument must be an array.');
+        }
+        if (x.length < 3) {
+            return x.join(' and anti-');
+        }
+        return x.slice(0, -1).join(', anti-') + ', and anti-' + x.slice(-1);
+    };
+        conjoinB = function (x) {
+     // This function joins the elements of an array using the "Oxford comma".
+        if ((x instanceof Array) === false) {
+            throw new TypeError('Argument must be an array.');
+        }
+        if (x.length < 3) {
+            return x.join('- and ');
+        }
+        return x.slice(0, -1).join('-, ') + '-, and ' + x.slice(-1);
+    };
+
     for(i=0;i<antibodies.length;i++)
    {
 //       matrix[i]="comp"+antibodies[i];
@@ -48,22 +75,36 @@ function processForm() {
     {
         document.antigenForm.textField.value = "";
     };
-    if(patientAntibodies.length === 1) 
+    if(document.antigenForm.antTiter.checked && numberAntibodies===1)
     {
-        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies+" alloantibodies. ";
-    };
-    if(patientAntibodies.length === 2) 
+        document.antigenForm.textField.value = "";
+         document.antigenForm.textField.value+="The patient has anti-"+patientAntibodies+" antibodies at a titer of " + document.antigenForm.titer.value +". ";
+    }
+    else if(patientAntibodies.length >= 1) 
     {
-        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies[0]+" and anti-"+ patientAntibodies[1] + " alloantibodies. ";
-    };
-    if(patientAntibodies.length === 3) 
+        document.antigenForm.textField.value = "The patient has anti-"+conjoin(patientAntibodies)+" alloantibodies. ";
+    }
+    
+    if(numberAntibodies>1 && antigen[0].checked && document.antigenForm.RhImmuno.checked)
     {
-        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies[0]+ ", anti-"+patientAntibodies[1]+", and anti-"+ patientAntibodies[2] + " alloantibodies. ";
+          document.antigenForm.textField.value += "The anti-D alloantibody is likely due to Rh immunoglobulin administration during this pregnancy. "; 
     };
-    if(patientAntibodies.length === 4) 
+    if(numberAntibodies>1 && antigen[15].checked)
     {
-        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies[0]+ ", anti-"+patientAntibodies[1]+", anti-"+patientAntibodies[2]+", and anti-"+ patientAntibodies[3] + " alloantibodies. ";
-    };
+            document.antigenForm.textField.value += "Anti-M can be naturally occurring and does not react at body temperature; thus, it is not clinically significant. "; 
+    }
+//    if(patientAntibodies.length === 2) 
+//    {
+//        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies[0]+" and anti-"+ patientAntibodies[1] + " alloantibodies. ";
+//    };
+//    if(patientAntibodies.length === 3) 
+//    {
+//        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies[0]+ ", anti-"+patientAntibodies[1]+", and anti-"+ patientAntibodies[2] + " alloantibodies. ";
+//    };
+//    if(patientAntibodies.length === 4) 
+//    {
+//        document.antigenForm.textField.value = "The patient has anti-"+patientAntibodies[0]+ ", anti-"+patientAntibodies[1]+", anti-"+patientAntibodies[2]+", and anti-"+ patientAntibodies[3] + " alloantibodies. ";
+//    };
 //    document.antigenForm.textField.value+="test";
     
     for(i = 0; i < 2;i++)
@@ -109,22 +150,22 @@ function processForm() {
         };  
     };
 
-    if(patientSexA !=0 && numberAntibodies===1) 
+    if(patientSexA !=0 && numberAntibodies>=1) 
     {
-        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " +patientAntibodies[0]+ "-antigen negative units. ";
+        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " + conjoinB(patientAntibodies)+ "-antigen negative units. ";
     };   
-        if(patientSexA !=0 && numberAntibodies===2) 
-    {
-        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " +patientAntibodies[0]+"- and "+patientAntibodies[1] +"-antigen negative units. ";
-    }; 
-    if(patientSexA !=0 && numberAntibodies===3) 
-    {
-        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " +patientAntibodies[0]+"-, "+patientAntibodies[1]+"- and "+patientAntibodies[2] +"-antigen negative units. ";
-    };   
-    if(patientSexA !=0 && numberAntibodies===4) 
-    {
-        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " + patientAntibodies[0]+"-, "+ patientAntibodies[1]+"-, "+patientAntibodies[2]+"- and "+patientAntibodies[3] +"-antigen negative units. ";
-    };   
+//        if(patientSexA !=0 && numberAntibodies===2) 
+//    {
+//        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " +patientAntibodies[0]+"- and "+patientAntibodies[1] +"-antigen negative units. ";
+//    }; 
+//    if(patientSexA !=0 && numberAntibodies===3) 
+//    {
+//        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " +patientAntibodies[0]+"-, "+patientAntibodies[1]+"- and "+patientAntibodies[2] +"-antigen negative units. ";
+//    };   
+//    if(patientSexA !=0 && numberAntibodies===4) 
+//    {
+//        document.antigenForm.textField.value+="If transfusion is necessary, the patient will receive extended crossmatch compatible " + patientAntibodies[0]+"-, "+ patientAntibodies[1]+"-, "+patientAntibodies[2]+"- and "+patientAntibodies[3] +"-antigen negative units. ";
+//    };   
  //     document.antigenForm.textField.value += "test1"; 
     for(i = 0; i < compUnitsA.length;i++ )
     {
@@ -139,10 +180,23 @@ function processForm() {
     {
         document.antigenForm.textField.value+="Less than 1 percent of donor units are expected to be compatible; considerable time is needed to procure units for this patient. ";
     };
+
+    if(numberAntibodies===1 && antigen[15].checked)
+    {
+           document.antigenForm.textField.value = "";
+           document.antigenForm.textField.value = "This patient has anti-M. This antibody can be naturally occurring and does not react at body temperature; thus, it is not clinically significant.  If transfusion is necessary, she will receive appropriate pre-warm extended crossmatch compatible units. "; 
+    };
+    if(numberAntibodies===1 && antigen[0].checked && document.antigenForm.RhImmuno.checked)
+    {
+           document.antigenForm.textField.value = "";
+           document.antigenForm.textField.value = "This patient has anti-D, likely due to Rh immunoglobulin administration during this pregnancy.  If transfusion is necessary, the Blood Bank will issue extended crossmatch compatible Rh negative units to prevent sensitization to the D antigen. "; 
+    };
 //    document.antigenForm.textField.value += Environment.NewLine;
    document.antigenForm.textField.value += "\r\n"; 
    document.antigenForm.textField.value += "\r\n"; 
    document.antigenForm.textField.value+="Consult performed by:"
+
+//   document.antigenForm.textField.value += conjoin(patientAntibodies);
 //  document.antigenForm.textField.value+=currentlyPregnant
  //    document.antigenForm.textField.value += "test"+percentCompatible; 
 //      document.antigenForm.textField.value += "test2";
@@ -151,3 +205,4 @@ function processForm() {
 //    document.antigenForm.textField.value += document.antigenForm.compLua.value;
 //    document.antigenForm.textField.value += percentCompatible;                  
  };
+
